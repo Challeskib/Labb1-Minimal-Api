a3ec3995202696174db2854c8167c11d0813a01b
Charlie Skibsted <charlieskibsted@gmail.com>
2023-09-22 14:13:09 +0200

    More dropdown menus that now works on the edit button

--- Models/DTOS/EditBookDto.cs
+++ Models/DTOS/EditBookDto.cs
@@ -8,6 +8,11 @@
         public int Year { get; set; }
         public bool LoanAble { get; set; }
         public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public IEnumerable<Author> Authors { get; set; }
+
+        public Genre Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
         public int GenreId { get; set; }
     }
 }
